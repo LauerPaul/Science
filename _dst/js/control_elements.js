a22bf9cc7ae2d4d749ce44c2cd5343b47feb034c
Pavel Uhrynovych <pu@markline.agency>
2017-05-16 18:07:05 +0300

    widget-calendar icons header

--- _dst/js/control_elements.js
+++ _dst/js/control_elements.js
@@ -1,24 +1,12 @@
+var log_status = true; // Логировать инициализацию и события (по умолчанию false)
 // =================================================================================
 // ============================= DOCUMENT READY  ===================================
 // =================================================================================
 $(document).ready(function() {
-	// -----------------------------------------------------------------------------
-	// ---------------------- CALENDAR FOR EVENTS WIDGET ---------------------------
-	if($("#events-calendar").length > 0){
-		var dates;
-		calendar_events.init($("#events-calendar"), 'ru', dates);
+	application.init(log_status);
+	if(log_status){
+		console.log('Инициализация документа - application.init');
 	}
-	// -----------------------------------------------------------------------------
-	// ----------------------BOOTSTRAP SELECT SETTINGS -----------------------------
-	$('.selectpicker').selectpicker({
-		iconBase: 'sceinceIcon'
-	});
-	// -----------------------------------------------------------------------------
-	// ------------------------BOOTSTRAP TOOLTIP INIT ------------------------------
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip();
-	})
-	// -----------------------------------------------------------------------------
 });
 // =================================================================================
 // ========================== *DOCUMENT READY END* =================================
@@ -30,8 +18,15 @@ $(document).ready(function() {
 $(document).on('click', '.bootstrap-select .dropdown-toggle', function(){
 	var _this = $(this),
 		parent = _this.parent('.bootstrap-select');
+	
+	if(log_status){
+		console.log('on click событие - .bootstrap-select .dropdown-toggle');
+	}	
 	if(parent.hasClass('open')){
 		$('div.dropdown-menu', parent).jScrollPane();
+		if(log_status){
+			console.log('Инициализация jScrollPane - .bootstrap-select .dropdown-toggle div.dropdown-menu');
+		}
 	}
 })
 // ------------------ *SCROLL STYLING BOOTSTRAP SELECT END* ------------------------
@@ -47,6 +42,10 @@ $(document).on('click', '.header-button-search', function(event){
 		width_block = $('.header-menu', parent).outerWidth() + $('.header-section-search', parent).outerWidth() - a,
 		close_button = $('.close-serch', search);
 
+	if(log_status){
+		console.log('on click событие - .header-button-search');
+	}	
+
 	hide_elements.animate({
 		'opacity': 0},
 		300, function() {
@@ -57,6 +56,9 @@ $(document).on('click', '.header-button-search', function(event){
 			search.hide('slow', function(){
 				hide_elements.animate({'opacity': 1},200);	
 			});
+			if(log_status){
+				console.log('on click событие - search.hide');
+			}
 		});
 	});
 });
@@ -82,6 +84,10 @@ $(document).on('changed.bs.select', '.select-parent#science .bootstrap-select', 
 		ajax_url = select_.attr('data-url')+val_+'/'+types_select.attr('data-url')+types_filter+'/', // *URL FOR AJAX*
 		wrapper_result = $('.publications-wrapper .result-wrapper'); // Wrapper for html results
 
+		if(log_status){
+			console.log('on change событие - #science filter');
+		}
+
 	// REMOVE
 	var  success = { 
 		list: [
@@ -91,7 +97,6 @@ $(document).on('changed.bs.select', '.select-parent#science .bootstrap-select', 
 		],
 		responsHTML: "Text"
 	}
-	console.log(val_);
 	//
 
 	// ------------ *success* ----------------
@@ -117,8 +122,11 @@ $(document).on('changed.bs.select', '.select-parent#science-themes .bootstrap-se
 		ajax_url = select_.attr('data-url')+val_+'/'+types_select.attr('data-url')+types_filter+'/', // *URL FOR AJAX*
 		wrapper_result = $('.publications-wrapper .result-wrapper'); // Wrapper for html results
 
+		if(log_status){
+			console.log('on change событие - #science-themes filter');
+		}
+
 	//remove
-	console.log(val_);
 	var results_ = '<b>Types selected:</b> ' + types_filter+'<br><b>Science:</b> '+science_filter+'<br><b>Theme:</b> '+val_+'<br><b>URL AJAX:</b> '+ajax_url ;
 	//
 	
@@ -142,6 +150,9 @@ $(document).on('changed.bs.select', '.select-parent#types .bootstrap-select', fu
 		science_themes_filter = themes_select.val(),// Science Themes filter value
 		wrapper_result = $('.publications-wrapper .result-wrapper'); // Wrapper for html results
 
+		if(log_status){
+			console.log('on change событие - #types filter');
+		}
 
 	if($(select.children('option')[0]).prop('selected')){
 		$(select.children('option')[0]).prop('selected', false);
@@ -168,6 +179,10 @@ $(document).on('show.bs.select', '.select-parent#types .bootstrap-select', funct
 	var parent = $('.select-parent#types'), // parent block
 		select = $('select.selectpicker', parent); // $('select')
 
+		if(log_status){
+			console.log('on show событие - #types filter');
+		}
+
 	if($(select.children('option')[0]).prop('selected')){
 		$('.dropdown-menu li a', parent).addClass('select-main');
 	}else{
@@ -181,33 +196,118 @@ $(document).on('show.bs.select', '.select-parent#types .bootstrap-select', funct
 // ========================== *FILTERS ACTION END* =================================
 // =================================================================================
 
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++ APPLICATION FUNCTIONS ++++++++++++++++++++++++++++++
+var application = {
+	init: function(log_status = false) {
+		// -----------------------------------------------------------------------------
+		// ---------------------- CALENDAR FOR EVENTS WIDGET ---------------------------
+		if($("#events-calendar").length > 0){
+			$(function (){
+				var dates = [
+						{
+							"date":"2017-05-08",
+							"badge":true,
+							"title":"Tonight",
+							"body":"<p class=\"lead\">Party<\/p><p>Like it's 1999.<\/p>",
+							"footer":"At Paisley Park",
+							"classname":"purple-event",
+						},
+						{
+							"date":"2017-05-20",
+							"badge":true,
+							"title":"Tonight",
+							"body":"<p class=\"lead\">Party<\/p><p>Like it's 1999.<\/p>",
+							"footer":"At Paisley Park",
+							"classname":"purple-event"
+						}
+					];
+				calendar_events.init($("#events-calendar"), 'ru', dates, log_status);
+				if(log_status){
+					console.log('Вызов скрипта календаря событий (виджет) - calendar_events.init');
+				}
+			});
+		}
+		// -----------------------------------------------------------------------------
+		// ----------------------BOOTSTRAP SELECT SETTINGS -----------------------------
+		$('.selectpicker').selectpicker({
+			iconBase: 'sceinceIcon',
+		});
+		if(log_status){
+			console.log('SelectPicker - иницализация');
+		}
+		// -----------------------------------------------------------------------------
+		// ------------------------BOOTSTRAP TOOLTIP INIT ------------------------------
+		$('[data-toggle="tooltip"]').tooltip();
+		if(log_status){
+			console.log('Bootstrap Tooltips - иницализация');
+		}
+		// -----------------------------------------------------------------------------
+	}
+},
+// +++++++++++++++++++++++++ *APPLICATION FUNCTIONS END* +++++++++++++++++++++++++++
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // +++++++++++++++++++++++++ PRINT RESULT AJAX FUNCTIONS +++++++++++++++++++++++++++
-var results = {
+results = {
 	// .html function 
 		html_: function (wrapper, text){
 			$(wrapper).html(text);
+			if(log_status){
+				console.log('Вывод результата Ajax в объект '+wrapper+' - results.html_');
+			}
 		},
 	// .print function 
 		print_: function (wrapper, text){
 			$(wrapper).html(text);
+			if(log_status){
+				console.log('Вывод результата Ajax в объект '+wrapper+' - results.print_');
+			}
 		} 
 }
 // ++++++++++++++++++++++ *PRINT RESULT AJAX FUNCTIONS END* ++++++++++++++++++++++++
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-var calendar_events = {
-	init: function(obj, lang, date){
+// +++++++++++++++++++++++ CALENDAR EVENTS WIDGET FUNCTIONS ++++++++++++++++++++++++
+calendar_events = {
+	// http://zabuto.com
+	init: function(obj, lang, date, log_status){
 		obj.zabuto_calendar({
 			/* de	Deutsch (German)	 	nl	Nederlands (Dutch)
 			 en	English [default value]	 	pt	Portugues (Portuguese)
 			 es	Espanol (Spanish)	 	ru	ру́сский язы́кR (Russian)
 			 fr	Francais (French)	 	se	Svenska (Swedish)
 			 it	Italiano (Italian)	 	tr	Türkçe (Turkish) */
-			language: lang,
 			data: date,
+			language: lang,
+			action: function () {
+                return calendar_events.myDateFunction(this.id, false);
+            },
+            action_nav: function (log_status) {
+                return calendar_events.myNavFunction(this.id);
+            },
 			nav_icon: {
 		        prev: '<i class="fa fa-chevron-circle-left"></i>',
-		        next: '<i class="fa fa-chevron-circle-right"></i>'}
+		        next: '<i class="fa fa-chevron-circle-right"></i>'},
 	    });
+	    if(log_status){
+			console.log('Инициализация календаря событий (виджет) - calendar_events.init');
+		}
+	},
+	myDateFunction: function(id, log_status) {
+		// Событие при выборе дня
+	  var date = $("#" + id).data("date");
+	  var hasEvent = $("#" + id).data("hasEvent");
+	},
+	myNavFunction: function(id, log_status) {
+		// Событие при перелистывании месяца
+	  var nav = $("#" + id).data("navigation");
+	  var to = $("#" + id).data("to");
 	}
 }
+// ++++++++++++++++++++ *CALENDAR EVENTS WIDGET FUNCTIONS END* +++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
